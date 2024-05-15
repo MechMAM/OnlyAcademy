4de79003e08016e7466b3bb6f4b73e5b4e152212
Miguel A. M. <engmiguelmonteiro@gmail.com>
2024-05-14 21:31:26 -0300

    chore: incluído package camera-roll

--- OnlyAcademyApp/src/App.tsx
+++ OnlyAcademyApp/src/App.tsx
@@ -2,15 +2,41 @@ import {View, Text} from 'react-native';
 import React from 'react';
 import {Button} from 'react-native-paper';
 
+// function TelaCamera() {
+//   const device = useCameraDevice('back');
+//   const {hasPermission} = useCameraPermission();
+
+//   if (!hasPermission) {
+//     return (
+//       <>
+//         <Text>Não há permissão</Text>
+//       </>
+//     );
+//   }
+//   // if (device == null) {
+//   //   return <NoCameraDeviceError />;
+//   // }
+//   return (
+//     <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+//   );
+// }
+
 export default function App() {
+  // const {requestPermission} = useCameraPermission();
+  // requestPermission();
+
+  // const cameraView = () => {
+  //   return <TelaCamera />;
+  // };
+
   return (
     <View>
       <Text>Hello World</Text>
       <Button
         icon="camera"
         mode="contained"
-        onPress={() => console.log('Pressed')}>
-        Press me
+        onPress={() => console.log('Botão pressionado')}>
+        Open camera
       </Button>
     </View>
   );
